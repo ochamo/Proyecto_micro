5613273637ce291b9280fb2fa9d7ae84207a6af6
ochamo <60203851+ochamo@users.noreply.github.com>
2022-11-05 21:58:13 -0600

    feat: Create assembly_functions.h

---
+++ assembly_functions.h
@@ -0,0 +1,10 @@
+#ifndef ASSEMBLY_FUNCTIONS_H
+#define ASSEMBLY_FUNCTIONS_H
+#endif
+
+#include <stdint.h>
+
+// funciones que luego seran definidas en assembler.
+
+extern void command_register_display(uint32_t *registerArray);
+extern void command_register_modify(int32_t registro, uint64_t data);
