0b499641434628480f01ad188739287245e94bc6
ochamo <60203851+ochamo@users.noreply.github.com>
2022-11-07 19:45:40 -0600

    add comamand utils

---
+++ command_utils.h
@@ -0,0 +1,20 @@
+#ifndef COMMAND_UTILS_H
+#define COMMAND_UTILS_H
+#endif
+
+// Aquí estan todos los comandos que soporta el proyecto
+
+const char *RD_COMMAND = "RD";
+const char *RM_COMMAND = "RM";
+const char *MD_COMMAND = "MD";
+const char *MM_COMMAND = "MM";
+const char *BF_COMMAND = "BF";
+const char *RUN_COMMAND = "RUN";
+const char *CALL_COMMAND = "CALL";
+const char *IOMAP_COMMAND = "IOMAP";
+const char *IOUNMAP_COMMAND = "IOUNMAP";
+const char *SOUND_COMMAND = "SOUND";
+const char *MUTE_COMMAND = "MUTE";
+const char *RGBOUT_COMMAND = "RGBOUT";
+const char *ARCADE_COMMAND = "ARCADE";
+const char *CLEAR_COMMAND = "CLEAR";
