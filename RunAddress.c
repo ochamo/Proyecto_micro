e05fc844134b1899d99d080a369e16518dbd46aa
Abel <abel.alvarez@galileo.edu>
2022-11-12 12:50:44 -0600

    Modi Run Address C

--- RunAddress.c
+++ RunAddress.c
@@ -1,4 +1,3 @@
-static char *RUN_ADDR;
 
 unsigned long run_addr_dir;
 
@@ -6,11 +5,13 @@ extern void ensamblador_run_address(uint32_t address);
 
 
 void RunAddress(void){
+    char *RUN_ADDR= parameters_commands[1];
+    char *run_address_indicador;
+    long run_addr_dir
 	USART2_putSTring("Run Address\n\r");
-	RUN_ADDR = parameters_commands[1];
 	//Removemos Character
 	character_remove(RUN_ADDR, 'x');
-	run_addr_dir = strtoul(RUN_ADDR, &pointer, 16);
+	run_addr_dir = strtoul(RUN_ADDR, &run_address_indicador, 16);
 	//Mandamos run_addr_dir a funcion en ensamblador
 	ensamblador_run_address(run_addr_dir);
 }
